bmistree/Waldo

commit a9350bb6d68e15b2b47e0ed633f1200bacb7bb79

Now permit empty message sequence sections.  Will make debugging much easier.

parser/ast/astBuilder_v2.py
@@ -51,14 +51,23 @@ def p_RootExpression(p):
 def p_MessageSequenceSection(p):
     '''MessageSequenceSection : MessageSequence MessageSequenceSection
                               | MessageSequence
+                              | Empty
     '''
-    
     p[0] = AstNode(AST_MESSAGE_SEQUENCE_SECTION,p[1].lineNo,p[1].linePos);
-    p[0].addChild(p[1]);
+    
+    if not isEmptyNode(p[1]):
+        p[0].addChild(p[1]);
     
     if (len(p) == 3):
         #flattens all message sequences into siblings
-        p[0].addChildren(p[2].getChildren());
+        kids = p[2].getChildren();
+        for kid in kids:
+            if isEmptyNode(kid):
+                continue;
+            p[0].addChild(kid);
+            
+        # p[0].addChildren(p[2].getChildren());
+
 
                    
 def p_MessageSequence(p):

parser/ast/astBuilderCommon.py
@@ -45,16 +45,17 @@ def p_TraceSection(p):
     #note: this is an intermediate production, and will get skipped.
     p[0] = AstNode(AST_TRACE_SECTION,p.lineno(1),p.lexpos(1));
 
-    if (len(p) == 4):
-        # throw error if no code in traces body section.
-        p[0].value = p[1]; # WaldoParseException requires a value field.
-        errMsg = '\nERROR: you must enter code into the "' + p[1] + '" ';
-        errMsg += 'section before you can compile.\n';
-        raise WaldoParseException(p[0],errMsg);
+    if len(p) == 4:
+        pass;
+        # # throw error if no code in traces body section.
+        # p[0].value = p[1]; # WaldoParseException requires a value field.
+        # errMsg = '\nERROR: you must enter code into the "' + p[1] + '" ';
+        # errMsg += 'section before you can compile.\n';
+        # raise WaldoParseException(p[0],errMsg);
 
-    
-    #getting TraceBodySection's children removes it as an intermediate node.
-    p[0].addChildren(p[3].getChildren());
+    else:
+        # getting TraceBodySection's children removes it as an intermediate node.
+        p[0].addChildren(p[3].getChildren());
 
 
 

commit a384bbbe2d06bcdc51157163e8fac387f1ba6f35

Commit #a9350bb accidentally caused a reduce/reduce conflict.  This commit should fix that.

parser/ast/astBuilder_v2.py
@@ -42,17 +42,27 @@ from astBuilderCommon import *
 
 
 def p_RootExpression(p):
-    'RootExpression : NameSection EndpointAliasSection TraceSection SharedSection MessageSequenceSection EndpointSection EndpointSection';
+    '''
+    RootExpression : NameSection EndpointAliasSection TraceSection SharedSection MessageSequenceSection EndpointSection EndpointSection
+                   | NameSection EndpointAliasSection TraceSection SharedSection EndpointSection EndpointSection
+    ''';
     
     p[0] = AstNode(AST_ROOT,p[1].lineNo,p[1].linePos);
-    p[0].addChildren([p[1],p[2],p[3],p[4],p[6],p[7], p[5]]);  # message section gets added to end
+
+    p[0].addChildren([p[1],p[2],p[3],p[4]]);
+    if len(p) == 8:
+        msgSeqSectionToAdd = p[5];
+        p[0].addChildren([p[6],p[7], msgSeqSectionToAdd]);  # message section gets added to end
+    else:
+        msgSeqSectionToAdd = AstNode(AST_MESSAGE_SEQUENCE_SECTION,p[4].lineNo,p[4].linePos);
+        p[0].addChildren([p[5],p[6], msgSeqSectionToAdd]);  # message section gets added to end
 
 
 def p_MessageSequenceSection(p):
     '''MessageSequenceSection : MessageSequence MessageSequenceSection
                               | MessageSequence
-                              | Empty
     '''
+    
     p[0] = AstNode(AST_MESSAGE_SEQUENCE_SECTION,p[1].lineNo,p[1].linePos);
     
     if not isEmptyNode(p[1]):