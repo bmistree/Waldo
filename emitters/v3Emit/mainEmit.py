01e9ab7836149b7a2753dd086860cfac9257efc2
bmistree <bmistree@stanford.edu>
2012-08-26 22:31:45 -0700

    Added emitting code for binary operations.

--- emitters/v3Emit/mainEmit.py
+++ emitters/v3Emit/mainEmit.py
@@ -73,6 +73,41 @@ def emit(astNode,fdepDict):
         
     elif astNode.label == AST_NUMBER:
         returner += astNode.value + ' ';
+
+    elif astNode.label == AST_LIST:
+        # handle list literal
+        returner += '[ ';
+        for child in astNode.children:
+            returner += emit(child,fdepDict);
+            returner += ', ';
+        returner += '] ';
+
+    elif astNode.label == AST_MAP:
+        # handle map literal
+        returner += '{ ';
+        for mapLiteralItem in astNode.children:
+            returner += emit(mapLiteralItem,fdepDict);
+            returner += ', ';
+        returner += '} ';
+        
+    elif astNode.label == AST_MAP_ITEM:
+        # individual entry of map literal
+        keyNode = astNode.children[0];
+        valNode = astNode.children[1];
+        
+        keyText = emit(keyNode,fdepDict);
+        valText = emit(valNode,fdepDict);
+        returner += keyText + ': ' + valText;
+
+    elif _isBinaryOperatorLabel(astNode.label):
+        lhsNode = astNode.children[0];
+        rhsNode = astNode.children[1];
+
+        lhsText = emit(lhsNode,fdepDict);
+        rhsText = emit(rhsNode,fdepDict);
+        operatorText = _getBinaryOperatorFromLabel(astNode.label);
+
+        returner += lhsText + operatorText + rhsText;
         
     else:
         errMsg = '\nBehram error: emitting for unknown label: ';
@@ -82,3 +117,59 @@ def emit(astNode,fdepDict):
 
 
     return returner;
+
+
+
+def _isBinaryOperatorLabel(nodeLabel):
+    '''
+    @param {String} nodeLabel --- The label field of an ast node.
+
+    @returns {Bool} True if the label represents a binary operation,
+    False otherwise.
+
+    '''
+    
+    if nodeLabel in _getBinaryOperatorLabelDict():
+        return True;
+    return False;
+
+
+def _getBinaryOperatorFromLabel(nodeLabel):
+    binOperatorDict =  _getBinaryOperatorLabelDict();
+    operator = binOperatorDict.get(nodeLabel,None);
+    if operator == None:
+        errMsg = '\nBehram error: requesting binary operator ';
+        errMsg += 'information for non-binary operator.\n';
+        print(errMsg);
+        assert(False);
+    return operator;
+
+def _getBinaryOperatorLabelDict():
+    '''
+    Helper function for _isBinaryOperatorLabel and
+    _getBinaryOperatorFromLabel
+    '''
+    binaryOperatorLabels = {
+        # arithmetic
+        AST_PLUS: '+',
+        AST_MINUS: '-',
+        AST_DIVIDE: '/',
+        AST_MULTIPLY: '*',
+
+        # boolean
+        AST_AND: 'and',
+        AST_OR: 'or',
+        
+        # comparison
+        AST_BOOL_EQUALS: '==',
+        AST_BOOL_NOT_EQUALS: '!=',
+        AST_GREATER_THAN: '>',
+        AST_GREATER_THAN_EQ: '>=',
+        AST_LESS_THAN: '<',
+        AST_LESS_THAN_EQ: '<='
+        };
+    return binaryOperatorLabels;
+
+
+
+
