42f549e5ef90a0e070becf3d4ab1d517609d2833
behram mistree <bmistree@stanford.edu>
2013-02-05 18:54:27 -0800

    Added several other basic value types.

--- emitters/v4Emit/lib/wObjects.py
+++ emitters/v4Emit/lib/wObjects.py
@@ -6,18 +6,32 @@ def initialize():
     Add separate type names to overall Waldo object.
     '''
     _WaldoObj.WALDO_TYPE_NAMES[WaldoNum.TYPE_WALDO_NUM] = True
-    # _WaldoObj.WALDO_TYPE_NAMES[_WaldoTrueFalse.TYPE_WALDO_TF] = True
-    # _WaldoObj.WALDO_TYPE_NAMES[_WaldoText.TYPE_WALDO_TEXT] = True
-    # _WaldoObj.WALDO_TYPE_NAMES[_WaldoList.TYPE_WALDO_LIST] = True
-    # _WaldoObj.WALDO_TYPE_NAMES[_WaldoMap.TYPE_WALDO_MAP] = True
+    _WaldoObj.WALDO_TYPE_NAMES[
+        WaldoTrueFalse.TYPE_WALDO_TRUE_FALSE] = True
+    _WaldoObj.WALDO_TYPE_NAMES[WaldoText.TYPE_WALDO_TEXT] = True
+    # _WaldoObj.WALDO_TYPE_NAMES[WaldoList.TYPE_WALDO_LIST] = True
+    # _WaldoObj.WALDO_TYPE_NAMES[WaldoMap.TYPE_WALDO_MAP] = True
 
         
 
+### VALUE TYPES
+    
 class WaldoNum(_WaldoObj):
     TYPE_WALDO_NUM = 'number'
 
     def __init__(self,init_val=0):
         _WaldoObj.__init__(self,WaldoNum.TYPE_WALDO_NUM,init_val)
-        
 
         
+class WaldoText(_WaldoObj):
+    TYPE_WALDO_TEXT = 'text'
+    
+    def __init__(self,init_val=''):
+        _WaldoObj.__init__(self,WaldoText.TYPE_WALDO_TEXT,init_val)
+        
+        
+class WaldoTrueFalse(_WaldoObj):
+    TYPE_WALDO_TRUE_FALSE = 'tf'
+    
+    def __init__(self,init_val=False):
+        _WaldoObj.__init__(self,WaldoTrueFalse.TYPE_WALDO_TRUE_FALSE,init_val)
---
+++ emitters/v4Emit/test/test_value_types.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+sys.path.append(
+    os.path.join('../lib/'))
+
+import wObjects
+import commitManager
+import invalidationListener
+import time
+
+class PrintTestInvalidationListener(invalidationListener._InvalidationListener):
+    
+    def notify_invalidated(self,wld_obj):
+        notify_msg = '\nWarning: ' + str(self.uuid)
+        notify_msg += ' got an invalidation for object '
+        notify_msg += str(wld_obj.uuid) + '.\n'
+        print notify_msg
+        
+        
+
+def setup():
+    wObjects.initialize()
+    commit_manager = commitManager._CommitManager()
+    evt1 = PrintTestInvalidationListener(commit_manager)
+    evt2 = PrintTestInvalidationListener(commit_manager)
+    number = wObjects.WaldoNum(31)
+    text = wObjects.WaldoText('ab')
+    tf = wObjects.WaldoText(True)
+
+    return evt1,evt2,number,text,tf
+    
+def run():
+    evt1,evt2,number,text,tf = setup()
+
+    print '\n\n'
+    print (number.get_val(evt1))
+    print '\n\n'
+    number.write_val(evt1,2)
+    text.write_val(evt1,'hi')
+    tf.write_val(evt1, False)
+    print (number.get_val(evt1))
+    print '\n\n'
+    print (number.get_val(evt2))
+    print (text.get_val(evt2))
+    print (tf.get_val(evt2))    
+    print '\n\n'    
+
+    if not evt1.hold_can_commit():
+        print '\nError: should be able to commit a single committer.\n'
+    else:
+        evt1.complete_commit()
+
+    time.sleep(2)
+
+    print (number.get_val(evt1))
+
+    
+    if evt2.hold_can_commit():
+        print '\nError: should have been unable to commit second event.\n'
+    else:
+        evt2.backout_commit(True)
+
+
+    print '\n\n'
+        
+
+if __name__ == '__main__':
+    run()
+
+
