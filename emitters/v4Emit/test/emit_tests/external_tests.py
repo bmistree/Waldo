7c772619a29200819967bf74249cc728118e6e51
bmistree <bmistree@stanford.edu>
2013-03-18 14:58:27 -0700

    Added a test to ensure that assigning an external ointo a bracket statement works.

--- emitters/v4Emit/test/emit_tests/external_tests.py
+++ emitters/v4Emit/test/emit_tests/external_tests.py
@@ -27,6 +27,8 @@ def test_ext_num(single_side):
         'garbage',single_side._host_uuid,False,original_num)
 
     single_side.input_ext_num(ext_num)
+    map_ext_index = 3
+    single_side.input_ext_num_to_map(ext_num,map_ext_index)
 
     NUM_INCREMENTS = 50
     for i in range(1,NUM_INCREMENTS):
@@ -39,12 +41,17 @@ def test_ext_num(single_side):
             print '\nErr: getting value'
             return False
 
+        if expected_num != single_side.get_endpoint_map_ext_value(map_ext_index):
+            print '\nErr: external value was not mirrored on map'
+            return False
+
 
     # test that can still get the original external back
     if ext_num.get_val(None) != single_side.get_endpoint_ext().get_val(None):
         print '\nErr: getting external back'
         return False
-        
+
+    
     return True
 
 
