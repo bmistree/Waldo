4b85a4b4165f67de068570ae159e024e155a0d36
bmistree <bmistree@stanford.edu>
2013-03-30 18:18:24 -0700

    Had a bug with deserializing messages: if received more than one tcp message at a time, would cut off first character of new message.  Fixed and added to test case testing for it.

--- emitters/v4Emit/test/ind_tests/tcp_encapsulate_decapsulate.py
+++ emitters/v4Emit/test/ind_tests/tcp_encapsulate_decapsulate.py
@@ -6,8 +6,9 @@ sys.path.append(
 import waldoConnectionObj
 
 NUM_RANDOM_STRINGS = 2000
+NUM_MULTI_MESSAGES = 500
 MAX_RAND_STRING_LEN = 5000
-
+MAX_NUM_IN_MULTI_MESSAGE = 10
 
 def get_random_string():
     string = ''
@@ -23,45 +24,75 @@ def get_string_list_to_test():
     @returns {list} --- Each element is a string that we try to encapsulate and decapsulate
     '''
     to_return = []
+    
     # put a bunch of random data in string list
     for i in range(0,NUM_RANDOM_STRINGS):
         rand_string = get_random_string()
         to_return.append(rand_string)
-    
+
     return to_return
+
+
+def decapsulate_all(to_decapsulate):
+    rest_of_str = 'a'
+    full_decapsulated = ''
+    while rest_of_str != '':
+        decapsulated,rest_of_str = waldoConnectionObj._WaldoTCPConnectionObj._decapsulate_msg_str(to_decapsulate)
+        full_decapsulated += decapsulated
+        to_decapsulate = rest_of_str
+
+    return full_decapsulated
+
+    print counter
+
+def check_err(decapsulated,original):
+    '''
+    @returns{bool} --- True if there is an error, False otherwise.
+    '''
+    if decapsulated != original:
+        filer = open('faulty.txt','w')
+        filer.write(original)
+        filer.flush()
+        filer.close()
+
+        err_msg = '\nErr: cannot encapsulate and decapsulate '
+        err_msg += 'string correctly.  Wrote faulty string to file '
+        err_msg += 'faulty.txt\n'
+        print err_msg
+        return True
+    return False
 
 
 def run_test():
     strings_to_test = get_string_list_to_test()
     for counter in range(0, len(strings_to_test)):
-        
         string = strings_to_test[counter]
         encapsulated = waldoConnectionObj._WaldoTCPConnectionObj._encapsulate_msg_str(string)
-        decapsulated,rest_of_str = waldoConnectionObj._WaldoTCPConnectionObj._decapsulate_msg_str(encapsulated)
+        decapsulated = decapsulate_all(encapsulated)
 
-        if decapsulated != string:
-            filer = open('faulty.txt','w')
-            filer.write(string)
-            filer.flush()
-            filer.close()
-
-            err_msg = '\nErr: cannot encapsulate and decapsulate '
-            err_msg += 'string correctly.  Wrote faulty strin to file '
-            err_msg += 'faulty.txt\n'
-            print err_msg
+        if check_err(decapsulated, string):
             return False
+
+
+    for i in range(0,NUM_MULTI_MESSAGES ):
+        num_concatenated_msgs = random.randint(1,MAX_NUM_IN_MULTI_MESSAGE)
+        expected_deserialized_msg = ''
+        serialized_msg = ''
+
+        # construct a message of many serialized strings
+        for j in range(0,num_concatenated_msgs):
+            single_string_index = random.randint(1,len(strings_to_test)) - 1
+            single_string = strings_to_test[single_string_index]
+            expected_deserialized_msg += single_string
+            serialized_msg += waldoConnectionObj._WaldoTCPConnectionObj._encapsulate_msg_str(
+                single_string)
+
+        
+        decapsulated = decapsulate_all(serialized_msg)
+        if check_err(decapsulated,expected_deserialized_msg):
+            return False
+        
     return True
-
-# def run_test():
-#     faulty_file = open('faulty.txt','r')
-#     faulty_str = faulty_file.read()
-#     encapsulated = waldoConnectionObj._WaldoTCPConnectionObj._encapsulate_msg_str(faulty_str)
-#     decapsulated,rest_of_str = waldoConnectionObj._WaldoTCPConnectionObj._decapsulate_msg_str(encapsulated)
-#     if decapsulated != faulty_str:
-#         print '\nGotcha!\n'
-    
-    
-
 
 
 if __name__ == '__main__':
