0b772a878b64808e087abae8db1ffd98f25f8ab2
bmistree <bmistree@stanford.edu>
2013-07-23 17:03:27 -0700

    Thin wrappers around Waldo objects so that can distinguish them with isinstance when performing operations on them.

---
+++ waldo/lib/waldoLockedVariables.py
@@ -0,0 +1,14 @@
+from waldoLockedObj import WaldoLockedObj
+
+
+class LockedValueVariable(WaldoLockedObj):
+    pass
+
+class LockedNumberVariable(LockedValueVariable):
+    pass
+
+class LockedTextVariable(LockedValueVariable):    
+    pass
+
+class LockedTrueFalseVariable(LockedValueVariable):
+    pass
