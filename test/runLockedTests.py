98c6461e4721bce452cd6f8c6a2325379ad87d58
bmistree <bmistree@stanford.edu>
2013-08-01 11:16:07 -0700

    Added infrastructure for running locked tests.

---
+++ test/runLockedTests.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import subprocess
+sys.path.append(
+    os.path.join('..','waldo','lib'))
+
+
+import locked_tests.chained_endpoint_call_no_conflict_test
+import locked_tests.endpoint_call_no_conflict_test
+import locked_tests.no_conflict_test
+import locked_tests.preempted_chained_endpoint_call
+import locked_tests.preemption_test
+import locked_tests.read_read_test
+
+
+def run_lib_tests():
+    to_run = [
+        ('Chained endpoint call with no conflict', locked_tests.chained_endpoint_call_no_conflict_test.run_test),
+        ('Single endpoint call with no conflict', locked_tests.endpoint_call_no_conflict_test.run_test),
+        ('Single event, no conflict', locked_tests.no_conflict_test.run_test),
+        ('Preempted, chained endpoint call', locked_tests.preempted_chained_endpoint_call.run_test),
+        ('Preempted single endpoint', locked_tests.preemption_test.run_test),
+
+        ('Concurrent reads', locked_tests.read_read_test.run_test),
+        ]
+    run_tests(to_run)
+
+def run_tests(to_run):
+    print ('\n\n')
+    for to_run_tuple in to_run:
+        test_name = to_run_tuple[0]
+        test_method = to_run_tuple[1]
+
+        print ('\nRunning test ' + test_name )
+        succeeded = test_method()
+        success_failure_msg = '.......'
+        if succeeded:
+            success_failure_msg += 'SUCCESS'
+        else:
+            success_failure_msg += 'FAILURE'
+
+        print (success_failure_msg)
+    print( '\n\n')
+
+    
+
+def run_all():
+    run_lib_tests()
+
+
+if __name__ == '__main__':
+    run_all()
