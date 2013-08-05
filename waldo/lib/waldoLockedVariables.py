9d1200e2bdf8122085752ca8fb5273b2b9b94b13
bmistree <bmistree@stanford.edu>
2013-08-05 14:51:36 -0700

    Added mechanism for initializing lists and maps with default values.

--- waldo/lib/waldoLockedVariables.py
+++ waldo/lib/waldoLockedVariables.py
@@ -2,6 +2,28 @@ from waldoLockedObj import WaldoLockedObj
 from waldoDataWrapper import ValueTypeDataWrapper
 from waldoDataWrapper import ReferenceTypeDataWrapper
 from waldoLockedContainer import WaldoLockedContainer
+import util
+
+def ensure_locked_obj(new_val,host_uuid):
+    '''
+    @param {Anything} new_val --- If new_val is a non-Waldo object,
+    convert it to a Waldo object.  Otherwise, return it unchanged.
+
+    This method is used to ensure that each individual entry in a
+    map/list is also protected.
+    '''
+    if isinstance(new_val , WaldoLockedObj):
+        return new_val
+
+    if isinstance(new_val, bool):
+        return LockedTrueFalseVariable(host_uuid,False,new_val)
+    elif isinstance(new_val, numbers.Number):
+        return LockedNumberVariable(host_uuid,False,new_val)
+    elif util.is_string(new_val):
+        return LockedTextVariable(host_uuid,False,new_val)
+    else:
+        util.logger_assert('Unknown object type.')
+    
 
 class LockedValueVariable(WaldoLockedObj):
     def __init__(self,host_uuid,peered,init_val):
@@ -27,6 +49,8 @@ class LockedMapVariable(WaldoLockedContainer):
         '''
         Map specific
         '''
+        new_val = ensure_locked_obj(new_val)
+        
         wrapped_val = self.acquire_write_lock(active_event)
         wrapped_val.val[key_added] = new_val
 
@@ -40,6 +64,7 @@ class LockedListVariable(WaldoLockedContainer):
         '''
         List specific
         '''
+        new_val = ensure_locked_obj(new_val)
         wrapped_val = self.acquire_write_lock(active_event)
         wrapped_val.val.insert(where_to_insert, new_val)
         
@@ -47,5 +72,6 @@ class LockedListVariable(WaldoLockedContainer):
         '''
         List specific
         '''
+        new_val = ensure_locked_obj(new_val)        
         wrapped_val = self.acquire_write_lock(active_event)
         wrapped_val.val.append(new_val)
