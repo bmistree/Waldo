d7d6a594b9db02873a8678323cd0acea192b7848
JayThomason <jayt92@stanford.edu>
2013-08-21 15:06:07 -0700

    Error type added to partnerError protobuf message.

--- waldo/lib/proto_compiled/partnerError_pb2.py
+++ waldo/lib/proto_compiled/partnerError_pb2.py
@@ -14,9 +14,30 @@ import util_pb2
 DESCRIPTOR = _descriptor.FileDescriptor(
   name='partnerError.proto',
   package='',
-  serialized_pb='\n\x12partnerError.proto\x1a\nutil.proto\"C\n\x0cPartnerError\x12\x19\n\nevent_uuid\x18\x01 \x02(\x0b\x32\x05.UUID\x12\x18\n\thost_uuid\x18\x02 \x02(\x0b\x32\x05.UUID')
+  serialized_pb='\n\x12partnerError.proto\x1a\nutil.proto\"\x95\x01\n\x0cPartnerError\x12\x19\n\nevent_uuid\x18\x01 \x02(\x0b\x32\x05.UUID\x12\x18\n\thost_uuid\x18\x02 \x02(\x0b\x32\x05.UUID\x12%\n\x04type\x18\x03 \x02(\x0e\x32\x17.PartnerError.ErrorType\")\n\tErrorType\x12\x0f\n\x0b\x41PPLICATION\x10\x01\x12\x0b\n\x07NETWORK\x10\x02')
 
 
+
+_PARTNERERROR_ERRORTYPE = _descriptor.EnumDescriptor(
+  name='ErrorType',
+  full_name='PartnerError.ErrorType',
+  filename=None,
+  file=DESCRIPTOR,
+  values=[
+    _descriptor.EnumValueDescriptor(
+      name='APPLICATION', index=0, number=1,
+      options=None,
+      type=None),
+    _descriptor.EnumValueDescriptor(
+      name='NETWORK', index=1, number=2,
+      options=None,
+      type=None),
+  ],
+  containing_type=None,
+  options=None,
+  serialized_start=143,
+  serialized_end=184,
+)
 
 
 _PARTNERERROR = _descriptor.Descriptor(
@@ -40,21 +61,31 @@ _PARTNERERROR = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
+    _descriptor.FieldDescriptor(
+      name='type', full_name='PartnerError.type', index=2,
+      number=3, type=14, cpp_type=8, label=2,
+      has_default_value=False, default_value=1,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
   ],
   extensions=[
   ],
   nested_types=[],
   enum_types=[
+    _PARTNERERROR_ERRORTYPE,
   ],
   options=None,
   is_extendable=False,
   extension_ranges=[],
-  serialized_start=34,
-  serialized_end=101,
+  serialized_start=35,
+  serialized_end=184,
 )
 
 _PARTNERERROR.fields_by_name['event_uuid'].message_type = util_pb2._UUID
 _PARTNERERROR.fields_by_name['host_uuid'].message_type = util_pb2._UUID
+_PARTNERERROR.fields_by_name['type'].enum_type = _PARTNERERROR_ERRORTYPE
+_PARTNERERROR_ERRORTYPE.containing_type = _PARTNERERROR;
 DESCRIPTOR.message_types_by_name['PartnerError'] = _PARTNERERROR
 
 class PartnerError(_message.Message):
