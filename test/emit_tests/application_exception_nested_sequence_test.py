59032c61ffb67220a1161e89e8fdc19953f28c72
JayThomason <jayt92@stanford.edu>
2013-08-21 15:44:34 -0700

    Renamed nested sequence waldo files for future re-use with nested passing of network exceptions. Refactored so that a foreign function is invoked to induce the exception/error.

--- test/emit_tests/application_exception_nested_sequence_test.py
+++ test/emit_tests/application_exception_nested_sequence_test.py
@@ -7,12 +7,18 @@ base_dir = os.path.join(
 sys.path.append(base_dir)
 
 from waldo.lib import Waldo
-from application_exception_nested_sequence_inner_v4 import InnerPing, InnerPong
-from application_exception_nested_sequence_outer_v4 import OuterPing, OuterPong
+from exception_nested_sequence_inner_v4 import InnerPing, InnerPong
+from exception_nested_sequence_outer_v4 import OuterPing, OuterPong
 
 HOST = 'localhost'
 PORT_INNER = 8777
 PORT_OUTER = 8778
+
+def throw_func():
+    '''
+    Throws an ApplicationException in Waldo.
+    '''
+    1 / 0
 
 def run_test():
     '''
@@ -24,7 +30,7 @@ def run_test():
     Returns true if the exception is propagated back to the root of the
     event and handled and false otherwise.
     '''
-    Waldo.tcp_accept(InnerPong, HOST, PORT_INNER)
+    Waldo.tcp_accept(InnerPong, HOST, PORT_INNER, throw_func)
     inner_ping = Waldo.tcp_connect(InnerPing, HOST, PORT_INNER)
     Waldo.tcp_accept(OuterPong, HOST, PORT_OUTER, inner_ping)
     outer_ping = Waldo.tcp_connect(OuterPing, HOST, PORT_OUTER)
