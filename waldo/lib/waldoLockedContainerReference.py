8cf14c3f4929a9e27215969646bc24d4c6d6751f
bmistree <bmistree@stanford.edu>
2013-08-21 18:47:36 -0700

    In addition to generating and incorporating deltas, switch to holding references to containers meant that we also had to override the references' de_waldoify method, which is used to change data back into python to be passed to external code.

--- waldo/lib/waldoLockedContainerReference.py
+++ waldo/lib/waldoLockedContainerReference.py
@@ -17,6 +17,9 @@ def incorporate_deltas(ref_obj,delta_to_incorporate,constructors,active_event):
     # FIXME: when allow peered data, will have to clean this up.
     internal_container = ref_obj.get_val(active_event)
     internal_container.incorporate_deltas(delta_to_incorporate,constructors,active_event)
+
+def de_waldoify(self,active_event):
+    return self.get_val(active_event).de_waldoify(active_event)
     
 class MultiThreadedContainerReference(LockedValueVariable):
     def serializable_var_tuple_for_network(self,parent_delta,var_name,active_event,force):
@@ -24,7 +27,10 @@ class MultiThreadedContainerReference(LockedValueVariable):
             self,parent_delta,var_name,active_event,force)
         
     def incorporate_deltas(self,delta_to_incorporate,constructors,active_event):
-        incorporate_deltas(self,delta_to_incorporate,constructors,active_event)
+        return incorporate_deltas(self,delta_to_incorporate,constructors,active_event)
+
+    def de_waldoify(self,active_event):
+        return de_waldoify(self,active_event)
         
 class SingleThreadedContainerReference(SingleThreadedLockedValueVariable):
     def serializable_var_tuple_for_network(self,parent_delta,var_name,active_event,force):
@@ -32,5 +38,7 @@ class SingleThreadedContainerReference(SingleThreadedLockedValueVariable):
             self,parent_delta,var_name,active_event,force)
         
     def incorporate_deltas(self,delta_to_incorporate,constructors,active_event):
-        incorporate_deltas(self,delta_to_incorporate,constructors,active_event)
+        return incorporate_deltas(self,delta_to_incorporate,constructors,active_event)
 
+    def de_waldoify(self,active_event):
+        return de_waldoify(self,active_event)
