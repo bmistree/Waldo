acc827fc0cae9baefc28c4a5e61f87aef4933f97
behram mistree <bmistree@stanford.edu>
2013-09-09 17:06:07 -0700

    Updated multithreaded test to actually perform concurrent reads on the same variable.

--- java/Waldo/src/library_tests/MultithreadedNoConflict.java
+++ java/Waldo/src/library_tests/MultithreadedNoConflict.java
@@ -29,38 +29,47 @@ public class MultithreadedNoConflict implements TestInterface{
 	{
 		String host_uuid = "host_uuid";
 		String multi_num = "multi_num";
-				
+			
+		double initial_value = 30;
 		VariableStore glob_var_store = new VariableStore(host_uuid);
-		LockedVariables.LockedNumberVariable num_var = new LockedVariables.LockedNumberVariable(host_uuid, false, 30);
+		LockedVariables.LockedNumberVariable num_var = new LockedVariables.LockedNumberVariable(host_uuid, false, initial_value);
 		glob_var_store.add_var(multi_num, num_var);
-		
 		test_util.SingleSidedEndpoint endpt = new test_util.SingleSidedEndpoint (host_uuid,glob_var_store);
 		
-		LockedActiveEvent active_event = null;
-		try {
-			active_event = endpt._act_event_map.create_root_event();
-		} catch (StoppedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-		Double read_value = null;
 		try
 		{
-			Number tmp = num_var.get_val(active_event);
-			read_value = tmp.doubleValue();
-		} catch (Exception e){}
-		
-		if (read_value != 30)
+			LockedActiveEvent read_event_one = endpt._act_event_map.create_root_event();
+			double read_value = num_var.get_val(read_event_one).doubleValue();
+			
+			if (read_value != initial_value)
+			{
+				System.out.println("Read incorrect value from num_var on event 1.");
+				return false;
+			}
+			
+			LockedActiveEvent read_event_two = endpt._act_event_map.create_root_event();
+			read_value = num_var.get_val(read_event_two).doubleValue();
+			if (read_value != initial_value)
+			{
+				System.out.println("Read incorrect value from num_var on event 2.");
+				return false;
+			}
+			
+			
+			read_event_one.begin_first_phase_commit();
+			read_event_two.begin_first_phase_commit();
+
+		}
+		catch (StoppedException e)
 		{
-			System.out.println("Read incorrect value from num_var");
+			e.printStackTrace();
 			return false;
 		}
-		
-		
-		active_event.begin_first_phase_commit();
-		
+		catch (BackoutException e)
+		{
+			e.printStackTrace();
+			return false;
+		}
 		
 		return true;
 	}
